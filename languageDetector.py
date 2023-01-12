d6a48ef135391aabd724f2c875b4032558e85dcc
Sufyan Qadir <yuske466@gmail.com>
2023-01-12 15:12:48 +1300

    Code added to filter out individual text blocks This version is much better than the first one, it even shows the page number and is fairly accurate

--- languageDetector.py
+++ languageDetector.py
@@ -9,4 +9,29 @@ from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.converter import PDFPageAggregator
 
 model = fasttext.load_model('Pdf-Proofing/lid.176.ftz')
-
+file= "Sample/Trimble_different.pdf"
+fp = open(file, 'rb')
+rsrcmgr = PDFResourceManager()
+laparams = LAParams()
+device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+interpreter = PDFPageInterpreter(rsrcmgr, device)
+pages = PDFPage.get_pages(fp)
+counter=0
+for page in pages:
+    counter+=1
+    interpreter.process_page(page)
+    layout = device.get_result()
+    for lobj in layout:
+        if isinstance(lobj, LTTextBox):
+            x, y, text = lobj.bbox[0], lobj.bbox[3], lobj.get_text()
+            text = text.replace('\n','')
+            pattern = r'[0-9]'
+            text = re.sub(pattern, '', text).replace('.', '')
+            if text !='':
+                lang = model.predict(text, k=1)[0][0].replace('__label__', '')
+                odds = model.predict(text, k=1)[1][0]
+                if lang!='it' and odds>0.95:
+                    print("page number : ",counter)
+                    print("language : ",lang)
+                    print("Text : ",text)
+                    print("")
